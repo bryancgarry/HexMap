b6a9205e886fe3c1d50033c739947130ab618e1e
Bryan Garry <bryan.c.garry@outlook.com>
2025-09-10 14:44:53 -0400

    Added Windows Support.

---
+++ build/CMakeFiles/HexMap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HexMap.dir/src/HexMap.cpp.o"
+  "CMakeFiles/HexMap.dir/src/HexMap.cpp.o.d"
+  "CMakeFiles/HexMap.dir/src/main.cpp.o"
+  "CMakeFiles/HexMap.dir/src/main.cpp.o.d"
+  "HexMap"
+  "HexMap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HexMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HexMap.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HexMap.
+# This may be replaced when dependencies are built.
